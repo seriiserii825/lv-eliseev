f2da94757fd86baa42959d0c08c08eb4650787a4
serii <seriiburduja@gmail.com>
2021-12-12 11:07:34 +0200

    removed docker mysql

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 7.30.5.
+ * Generated for Laravel 7.30.6.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,15 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="card-body">
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
 
-        <h2>Our site</h2>
-    </div>
-</div>
-@endsection
+<body>
+    <h2>Home</h2>
+</body>
+
+</html>
