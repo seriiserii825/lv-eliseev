772b3f628988e044328e68674b4b24b96536e502
serii <seriiburduja@gmail.com>
2021-12-13 09:07:11 +0200

    homeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,6 +14,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $users = User::all();
+        dd($users);
         return view('home');
     }
 }
