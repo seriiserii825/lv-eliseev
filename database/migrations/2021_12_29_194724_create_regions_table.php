6fe2b3d9c53647a96eff740302dbc649c23914d8
serii <seriiburduja@gmail.com>
2021-12-30 09:57:23 +0200

    regions migration

--- database/migrations/2021_12_29_194724_create_regions_table.php
+++ database/migrations/2021_12_29_194724_create_regions_table.php
@@ -14,7 +14,7 @@ class CreateRegionsTable extends Migration
     public function up()
     {
         Schema::create('regions', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name')->index();
             $table->string('slug');
             $table->integer('parent_id')->unsigned()->nullable();
