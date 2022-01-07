754f86d14b73955a84cba7c84c34d4a57f78e1b1
serii <seriiburduja@gmail.com>
2022-01-07 12:56:54 +0200

    category seed

--- database/seeds/AdvertsCategoriesTableSeeder.php
+++ database/seeds/AdvertsCategoriesTableSeeder.php
@@ -7,7 +7,7 @@ class AdvertsCategoriesTableSeeder extends Seeder
 {
     public function run()
     {
-        factory(AdvertsCategory::class, 10)->create()->each(function (AdvertsCategory $advertsCategory) {
+        factory(AdvertsCategory::class, 4)->create()->each(function (AdvertsCategory $advertsCategory) {
             $counts = [0, random_int(1, 3)];
             $advertsCategory->children()->saveMany(factory(AdvertsCategory::class, $counts[array_rand($counts)])->create()->each(function (AdvertsCategory $advertsCategory) {
                 $counts = [0, random_int(1, 3)];
